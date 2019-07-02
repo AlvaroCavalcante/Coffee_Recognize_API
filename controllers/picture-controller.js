702de40cef272173a2ba394c725d51f049f744df
LucasBrito <lucasbsilva29@gmail.com>
2019-07-01 23:42:03 -0300

    Aplicando conceitos de promisse

--- controllers/picture-controller.js
+++ controllers/picture-controller.js
@@ -15,27 +15,36 @@ exports.uploadAnexo = (req, res, next) => {
 };
 
 exports.processImage = (req, res, next) => {
-    try {
-        const pythonProcess = spawn('python3', ["scripts/detection_images.py"]);
-        //result.pid = pythonProcess.pid
-
-        console.log(`--------------- SERVIÇO INICIADO ---------------------\n`);
-
-        pythonProcess.stdout.on('data', (data) => {
-            console.log(`${data}`);
-        });
-
-
-        pythonProcess.on('close', (code) => {
-            console.log(`--------------- SERVIÇO CONCLUÍDO ---------------------\nCÓDIGO: ${code}`);
-
-        });
-
-    } catch (error) {
-        console.log(`-------------- ERRO -----------------\n${error}`);
-    }
-
-    return res.status(201).json({
-        message: 'sucess',
+    processRecognition().then(() => {
+        return res.status(201).json({ message: 'sucess' });
+    }).catch(error => {
+        return res.status(500).json({ message: error });
     });
 };
+
+function processRecognition() {
+    const promise = new Promise((resolve, reject) => {
+        try {
+            const pythonProcess = spawn('python3', ["scripts/detection_images.py"]);
+            //result.pid = pythonProcess.pid
+
+            console.log(`--------------- SERVIÇO INICIADO ---------------------\n`);
+
+            pythonProcess.stdout.on('data', (data) => {
+                console.log(`${data}`);
+                // reject();
+            });
+
+
+            pythonProcess.on('close', (code) => {
+                console.log(`--------------- SERVIÇO CONCLUÍDO ---------------------\nCÓDIGO: ${code}`);
+                resolve();
+            });
+
+        } catch (error) {
+            console.log(`-------------- ERRO -----------------\n${error}`);
+            reject();
+        };
+    });
+    return promise;
+}
